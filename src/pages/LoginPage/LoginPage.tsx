1d4b4daab072411f3d74d6f62a0845920debc3f3
lucas <santanafx@hotmail.com>
2024-10-29 19:54:13 -0300

    feat: create login template

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,3 +1,28 @@
+import { Button, TextField, Typography } from "@mui/material";
+import { LoginPageTemplate } from "../../components/templates/LoginPageTemplate/LoginPageTemplate";
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <LoginPageTemplate
+      mainText={
+        <Typography variant="h2" color="text.primary">
+          Login
+        </Typography>
+      }
+      emailInput={<TextField required id="outlined-required" label="Email" />}
+      passwordInput={
+        <TextField required id="outlined-required" label="Password" />
+      }
+      signInButton={
+        <Button variant="contained" size="large">
+          Sign in
+        </Button>
+      }
+      signUpButton={
+        <Button variant="contained" size="large">
+          Sign up
+        </Button>
+      }
+    />
+  );
 };
