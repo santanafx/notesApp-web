03ebf726ca0d611609555e195be9b429c3bb6f44
lucas <santanafx@hotmail.com>
2024-10-29 19:54:35 -0300

    feat: new interface

---
+++ src/components/templates/LoginPageTemplate/LoginPageTemplate.interface.ts
@@ -0,0 +1,7 @@
+export interface LoginPageTemplateProps {
+  mainText: React.ReactNode;
+  emailInput: React.ReactNode;
+  passwordInput: React.ReactNode;
+  signInButton: React.ReactNode;
+  signUpButton: React.ReactNode;
+}
