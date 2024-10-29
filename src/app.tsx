4e93df4549954bf2ed727533b7564cef20beabe0
lucas <santanafx@hotmail.com>
2024-10-29 19:52:07 -0300

    feat: added theme

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,24 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./global.css";
 import { CreateAccountPage } from "./pages/CreateAccountPage/CreateAccountPage";
 import { HomePage } from "./pages/HomePage/HomePage";
 import { LoginPage } from "./pages/LoginPage/LoginPage";
+import { themeOptions } from "./theme";
+
+const theme = createTheme(themeOptions);
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/createAccount" element={<CreateAccountPage />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/createAccount" element={<CreateAccountPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
