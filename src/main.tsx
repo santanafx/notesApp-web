dd896aa86d476562c5d209cdacecf7a3ae079f67
lucas <santanafx@hotmail.com>
2024-10-29 19:52:47 -0300

    refactor: removed theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
-import { ThemeProvider, createTheme } from "@mui/material/styles";
 import App from "./app";
-import "./global.css";
-import { themeOptions } from "./theme";
-
-const theme = createTheme(themeOptions);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>
 );
