27361162040a4f6f4f65bb043b302fe492f3bb37
lucas <santanafx@hotmail.com>
2024-10-31 07:28:09 -0300

    feat: new interface file

---
+++ src/components/templates/CreateAccountPageTemplate/CreateAccountPageTemplate.interface.ts
@@ -0,0 +1,6 @@
+export interface CreateAccountPageTemplateProps {
+  mainText: React.ReactNode;
+  emailInput: React.ReactNode;
+  passwordInput: React.ReactNode;
+  createAccountButton: React.ReactNode;
+}
