4bb0d92ee155f32d917380d4e18f3ccf3bf776db
lucas <santanafx@hotmail.com>
2024-10-31 07:29:01 -0300

    feat: added routes and password input

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,33 @@
-import { Button, TextField, Typography } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import {
+  Button,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { LoginPageTemplate } from "../../components/templates/LoginPageTemplate/LoginPageTemplate";
 
 export const LoginPage = () => {
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
+  const handleMouseUpPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
   return (
     <LoginPageTemplate
       mainText={
@@ -9,17 +35,50 @@ export const LoginPage = () => {
           Login
         </Typography>
       }
-      emailInput={<TextField required id="outlined-required" label="Email" />}
+      emailInput={
+        <TextField
+          required
+          id="outlined-required"
+          label="Email"
+          sx={{ width: 258 }}
+        />
+      }
       passwordInput={
-        <TextField required id="outlined-required" label="Password" />
+        <TextField
+          id="outlined-adornment-password"
+          type={showPassword ? "text" : "password"}
+          label="Password"
+          size="medium"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label={
+                    showPassword ? "hide the password" : "display the password"
+                  }
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  onMouseUp={handleMouseUpPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
       }
       signInButton={
-        <Button variant="contained" size="large">
+        <Button variant="contained" size="large" onClick={() => navigate("/")}>
           Sign in
         </Button>
       }
       signUpButton={
-        <Button variant="contained" size="large">
+        <Button
+          variant="contained"
+          size="large"
+          onClick={() => navigate("/createAccount")}
+        >
           Sign up
         </Button>
       }
